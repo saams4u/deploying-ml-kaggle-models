a4bd24b444d9233104e4de11109fd47fae8ff336
saams4u <mahjouri.saamahn@gmail.com>
2020-02-10 01:49:43 -0800

    more docker changes

--- .scripts/constants.py
+++ .scripts/constants.py
@@ -1,3 +1,3 @@
-HEROKU_API_KEY=556c42ee70cffbeea67c7393435cabc3
+HEROKU_API_KEY="556c42ee70cffbeea67c7393435cabc3"
 HEROKU_APP_NAME=kaggle-ml-api
-HEROKU_EMAIL=mahjouri.saamahn@gmail.com
+HEROKU_EMAIL="mahjouri.saamahn@gmail.com"
