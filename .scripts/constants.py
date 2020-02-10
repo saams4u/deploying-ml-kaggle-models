a2c9346cfabea6d5088faf410593860e828bdd61
saams4u <mahjouri.saamahn@gmail.com>
2020-02-10 10:05:24 -0800

    more docker changes

--- .scripts/constants.py
+++ .scripts/constants.py
@@ -1,3 +1,3 @@
-HEROKU_API_KEY="0a59e2e2111a12c159fdee5eecde2af5"
+HEROKU_API_KEY="572781ae-5436-4fc3-8000-c3d4cbd3bced"
 HEROKU_APP_NAME="kaggle-ml-api"
 HEROKU_EMAIL="mahjouri.saamahn@gmail.com"
--- .scripts/set_kaggle_creds.sh
+++ .scripts/set_kaggle_creds.sh
@@ -1,5 +1,2 @@
-# set KAGGLE_USERNAME=saams4u
-# set KAGGLE_KEY=556c42ee70cffbeea67c7393435cabc3
-
-set KAGGLE_USERNAME=sebastianbludd 
-set KAGGLE_KEY=0a59e2e2111a12c159fdee5eecde2af5
+set KAGGLE_USERNAME=saams4u
+set KAGGLE_KEY=572781ae-5436-4fc3-8000-c3d4cbd3bced
