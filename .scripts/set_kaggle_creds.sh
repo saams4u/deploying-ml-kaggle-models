9abe3dbfe07637ed537602f17a4da1f78e67d2f3
saams4u <mahjouri.saamahn@gmail.com>
2020-02-09 19:14:29 -0800

    recreated virtual environment

--- .scripts/set_kaggle_creds.sh
+++ .scripts/set_kaggle_creds.sh
@@ -1,2 +1,5 @@
-set KAGGLE_USERNAME=saams4u
-set KAGGLE_KEY=556c42ee70cffbeea67c7393435cabc3
+# set KAGGLE_USERNAME=saams4u
+# set KAGGLE_KEY=556c42ee70cffbeea67c7393435cabc3
+
+set KAGGLE_USERNAME=sebastianbludd 
+set KAGGLE_KEY=cbb71e7c55dc625feb6b59f9e40c0931
