1e2e1859191348f4202dd040d69bd96f7a0bc03e
saams4u <mahjouri.saamahn@gmail.com>
2020-02-10 02:02:14 -0800

    more docker changes

--- .scripts/set_kaggle_creds.sh
+++ .scripts/set_kaggle_creds.sh
@@ -2,4 +2,4 @@
 # set KAGGLE_KEY=556c42ee70cffbeea67c7393435cabc3
 
 set KAGGLE_USERNAME=sebastianbludd 
-set KAGGLE_KEY=cbb71e7c55dc625feb6b59f9e40c0931
+set KAGGLE_KEY=0a59e2e2111a12c159fdee5eecde2af5
